b4896e1392eef71c4a02caebc85e5aade48b11ba
Yagmur Kaya <kyagmur911@gmail.com>
2023-10-24 17:25:11 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from car import Car
+car_1=Car('reno','corvette','2024','blue')
+car_2=Car('Ford','Mustang',2022,'Red')
+
+print(car_1.make)
+print(car_1.model)
+print(car_1.year)
+print(car_1.color)
+
+car_1.drive()
+car_1.stop()
+
